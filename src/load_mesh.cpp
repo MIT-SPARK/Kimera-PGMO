b3654ebedcbeb75575d9801ece16b3e22f13e558
yunchang <yunchang@mit.edu>
2020-05-22 00:54:39 -0400

    visualizing mesh with colorized_mesh package

--- src/load_mesh.cpp
+++ src/load_mesh.cpp
@@ -3,9 +3,11 @@
  * @brief  Demo for mesh compression
  * @author Yun Chang
  */
+#include <colorized_mesh_display/ColorizedMesh.h>
+#include <colorized_mesh_display/ColorizedMeshStamped.h>
+#include <colorized_mesh_display/utils.h>
 #include <pcl/PolygonMesh.h>
 #include <pcl_conversions/pcl_conversions.h>
-#include <pcl_msgs/PolygonMesh.h>
 #include <ros/ros.h>
 
 #include "mesher_mapper/CommonFunctions.h"
@@ -19,16 +21,20 @@ int main(int argc, char* argv[]) {
   pcl::PolygonMeshPtr input_mesh(new pcl::PolygonMesh);
   ReadMeshFromPly(argv[1], input_mesh);
   ros::NodeHandle n;
-  ros::Publisher mesh_pub = n.advertise<pcl_msgs::PolygonMesh>("mesh", 10);
+  ros::Publisher mesh_pub =
+      n.advertise<colorized_mesh_display::ColorizedMeshStamped>("mesh", 10);
 
   ros::Rate loop_rate(10);
 
-  pcl_msgs::PolygonMesh mesh_msg;
-  pcl_conversions::moveFromPCL(*input_mesh, mesh_msg);
+  colorized_mesh_display::ColorizedMesh mesh_msg =
+      colorized_mesh_display::fromPCLPolygonMesh(*input_mesh);
 
   while (ros::ok()) {
-    mesh_msg.header.stamp = ros::Time::now();
-    mesh_pub.publish(mesh_msg);
+    colorized_mesh_display::ColorizedMeshStamped new_msg;
+    new_msg.header.stamp = ros::Time::now();
+    new_msg.header.frame_id = "world";
+    new_msg.mesh = mesh_msg;
+    mesh_pub.publish(new_msg);
 
     ros::spinOnce();
     loop_rate.sleep();
