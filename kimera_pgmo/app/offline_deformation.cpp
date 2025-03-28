3910a05fb0f9bebd470df96fa5cddaaaf8826d1b
Yun Chang <yunchang@mit.edu>
2025-03-28 11:07:17 -0400

    fix PR comment on offline deformation app

--- kimera_pgmo/app/offline_deformation.cpp
+++ kimera_pgmo/app/offline_deformation.cpp
@@ -1,17 +1,13 @@
 #include <config_utilities/config.h>
-#include <config_utilities/parsing/yaml.h>
+#include <config_utilities/parsing/context.h>
 #include <config_utilities/printing.h>
 #include <config_utilities/validation.h>
 #include <config_utilities/virtual_config.h>
-#include <gflags/gflags.h>
 #include <glog/logging.h>
 #include <kimera_pgmo/deformation_graph.h>
 #include <kimera_pgmo/optimizer/kimera_rpgo_optimizer.h>
 
 #include <filesystem>
-DEFINE_string(dgrf, "", "dgrf file to read");
-DEFINE_string(config, "", "path to config to use");
-DEFINE_string(log_path, "", "dgrf file to read");
 
 namespace kimera_pgmo {
 
@@ -56,8 +52,10 @@ class OfflineDeformation {
     deformation_graph_->updateTempValues(temp_estimates);
     deformation_graph_->updateInlierWeights(inlier_weights);
     deformation_graph_->updateTempInlierWeights(temp_inlier_weights);
-    deformation_graph_->save(config_.log_path);
-    LOG(INFO) << "Saved deformation optimization result to " << config_.log_path;
+    if (!config_.log_path.empty()) {
+      deformation_graph_->save(config_.log_path);
+      LOG(INFO) << "Saved deformation optimization result to " << config_.log_path;
+    }
   }
 
  private:
@@ -69,22 +67,20 @@ class OfflineDeformation {
 }  // namespace kimera_pgmo
 
 int main(int argc, char* argv[]) {
-  FLAGS_minloglevel = 0;
-  FLAGS_logtostderr = 1;
-  FLAGS_colorlogtostderr = 1;
+  /*
+   * Usage: ./offline_deformation path/to/dgrf [path/to/output/folder]
+   * --config-utilities-file path/to/config
+   */
+  config::initContext(argc, argv, true);
 
-  google::ParseCommandLineFlags(&argc, &argv, true);
-  google::InitGoogleLogging(argv[0]);
+  auto config =
+      config::checkValid(config::fromContext<kimera_pgmo::OfflineDeformationConfig>());
 
-  YAML::Node node;
-  std::filesystem::path config_path = FLAGS_config;
-  if (!config_path.empty() && std::filesystem::exists(config_path)) {
-    node = YAML::LoadFile(config_path);
+  config.dgrf_file = argv[1];
+
+  if (argc > 2) {
+    config.log_path = argv[2];
   }
-
-  auto config = config::fromYaml<kimera_pgmo::OfflineDeformationConfig>(node);
-  config.dgrf_file = FLAGS_dgrf;
-  config.log_path = FLAGS_log_path;
 
   kimera_pgmo::OfflineDeformation module(config);
   module.run();
