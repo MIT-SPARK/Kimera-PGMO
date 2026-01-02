93e57df3636acb1dfa3a8825b7ae916f8f945ce3
Nathan Hughes <nathan.h.hughes@gmail.com>
2026-01-02 10:36:42 -0500

    test mesh offsets remapping

--- kimera_pgmo/src/mesh_offset_info.cpp
+++ kimera_pgmo/src/mesh_offset_info.cpp
@@ -2,6 +2,8 @@
 
 #include <stdexcept>
 
+#include "kimera_pgmo/utils/logging.h"
+
 namespace kimera_pgmo {
 
 using IndexVec = std::vector<size_t>;
@@ -36,6 +38,7 @@ size_t MeshOffsetInfo::toLocalVertex(size_t global_idx) const {
 std::optional<size_t> MeshOffsetInfo::remapGlobalVertex(size_t index) const {
   if (!prev_to_curr_) {
     // no remapping -> no need to do work
+    SPARK_LOG(DEBUG) << "No remapping when trying to remap " << index;
     return index;
   }
 
--- kimera_pgmo/test/suites/test_mesh_offset_info.cpp
+++ kimera_pgmo/test/suites/test_mesh_offset_info.cpp
@@ -3,13 +3,26 @@
 #include "kimera_pgmo/mesh_offset_info.h"
 
 namespace kimera_pgmo {
+namespace {
 
-TEST(MeshOffsetInfo, LocalGlobalCorrect) {
-  MeshOffsetInfo offsets{5, 2, 10};
-  EXPECT_EQ(offsets.toGlobalVertex(0), 2u);
-  EXPECT_EQ(offsets.toLocalVertex(2), 0u);
+MeshOffsetInfo makeOffsets(size_t curr_archived,
+                           size_t prev_archived,
+                           size_t archived_faces,
+                           const std::map<size_t, size_t>& remapping = {}) {
+  return {curr_archived,
+          prev_archived,
+          archived_faces,
+          remapping.empty() ? nullptr
+                            : std::make_shared<std::map<size_t, size_t>>(remapping)};
+}
 
-  EXPECT_THROW({ offsets.toLocalVertex(0); }, std::logic_error);
+}  // namespace
+
+bool operator==(const MeshOffsetInfo::RemapStats& lhs,
+                const MeshOffsetInfo::RemapStats& rhs) {
+  return lhs.min_index == rhs.min_index && lhs.max_index == rhs.max_index &&
+         lhs.all_archived == rhs.all_archived &&
+         lhs.deleted_indices == rhs.deleted_indices;
 }
 
 TEST(MeshOffsetInfo, RemapTrackingCorrect) {
@@ -26,6 +39,51 @@ TEST(MeshOffsetInfo, RemapTrackingCorrect) {
   stats.addIndex(3);
   EXPECT_EQ(stats.min_index, 3u);
   EXPECT_EQ(stats.max_index, 10u);
+}
+
+TEST(MeshOffsetInfo, LocalGlobalCorrect) {
+  MeshOffsetInfo offsets{5, 2, 10};
+  EXPECT_EQ(offsets.toGlobalVertex(0), 2u);
+  EXPECT_EQ(offsets.toLocalVertex(2), 0u);
+
+  EXPECT_THROW({ offsets.toLocalVertex(0); }, std::logic_error);
+}
+
+TEST(MeshOffsetInfo, RemapVertexCorrect) {
+  std::map<size_t, size_t> remap{{0, 2}, {1, 1}, {2, 0}, {4, 3}, {5, 5}, {6, 4}};
+  const auto offsets = makeOffsets(8, 5, 2, remap);
+  std::vector<std::optional<size_t>> expected{7, 6, 5, std::nullopt, 8, 10, 9};
+  for (size_t i = 5; i < 12; ++i) {
+    const auto result = offsets.remapGlobalVertex(i);
+    EXPECT_EQ(result, expected[i - 5]) << "i: " << i;
+  }
+}
+
+TEST(MeshOffsetInfo, RemapIndicesVectorCorrect) {
+  std::map<size_t, size_t> remap{{0, 2}, {1, 1}, {2, 0}, {4, 3}, {5, 5}, {6, 4}};
+  const auto offsets = makeOffsets(8, 5, 2, remap);
+
+  const std::vector<size_t> expected{7, 6, 5, 8, 10, 9};
+  const MeshOffsetInfo::RemapStats expected_stats{5, 10, false, {3}};
+
+  std::vector<size_t> original{5, 6, 7, 8, 9, 10, 11};
+  MeshOffsetInfo::RemapStats result_stats;
+  EXPECT_EQ(offsets.remapVertexIndices(original, &result_stats), expected);
+  EXPECT_EQ(result_stats, expected_stats);
+}
+
+TEST(MeshOffsetInfo, RemapIndicesListCorrect) {
+  std::map<size_t, size_t> remap{{0, 2}, {1, 1}, {2, 0}, {4, 3}, {5, 5}, {6, 4}};
+  const auto offsets = makeOffsets(8, 5, 2, remap);
+
+  const std::list<size_t> expected{7, 6, 5, 8, 10, 9};
+  const MeshOffsetInfo::RemapStats expected_stats{5, 10, false, {3}};
+
+  std::list<size_t> result{5, 6, 7, 8, 9, 10, 11};
+  MeshOffsetInfo::RemapStats result_stats;
+  offsets.remapVertexIndices(result, &result_stats);
+  EXPECT_EQ(result, expected);
+  EXPECT_EQ(result_stats, expected_stats);
 }
 
 }  // namespace kimera_pgmo
